909acb81159d94b9c2cba640eef87c30328fd2f8
yagi0308 <choodee@icloud.com>
2025-01-20 09:40:25 +0900

    ログイン・ログアウトの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'prototypes/index'
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "prototypes#index"
+  resources :prototypes
 end
